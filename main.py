a9cb21592ffc88382e8b3adb4fda52afdb7ccb81
Anuj Kumar <anuj.kumar@mindmapdigital.ai>
2026-04-13 18:05:55 +0530

    completed the setup for all the imp services

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from smolagents import CodeAgent, InferenceClientModel
+
+model_id = "meta-llama/Llama-3.3-70B-Instruct"
+
+agent = CodeAgent(model=InferenceClientModel(model_id=model_id), add_base_tools=True)
+
+agent.run(
+    "Why does Mike not know many people in New York?",
+    additional_args={"mp3_sound_file_url":'https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/recording.mp3'}
+)
 def main():
     print("Hello from pm-bot!")
 
